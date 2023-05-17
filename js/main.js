64806c0ac32bc4c505903c61ea2ebe817e2942d5
Tyler Vick <vick.tyler@gmail.com>
2023-05-17 09:58:44 -0400

    Activate/deactivate major scale components

--- js/main.js
+++ js/main.js
@@ -23,8 +23,14 @@ const instruments = {
 };
 
 const canvasStyles = {
-  canvasWidth: 1200,
-  canvasHeight: 300,
+  fretboard: {
+    width: 1200,
+    height: 300,
+  },
+  piano: {
+    width: 1200,
+    height: 150,
+  }
 };
 
 const fretboardStyles = {
@@ -34,11 +40,43 @@ const fretboardStyles = {
   stringLength: 1500,
 };
 
+const chordPositions = {
+  major: [
+    {label: "R", position: 0},
+    {label: "2", position: 2},
+    {label: "3", position: 4},
+    {label: "4", position: 5},
+    {label: "5", position: 7},
+    {label: "6", position: 9},
+    {label: "7", position: 11},
+    {label: "R", position: 12},
+  ],
+  minor: [
+    {label: "R", position: 0},
+    {label: "2", position: 2},
+    {label: "3", position: 3},
+    {label: "4", position: 5},
+    {label: "5", position: 7},
+    {label: "6", position: 8},
+    {label: "7", position: 10},
+    {label: "R", position: 12},
+  ]
+}
+
+const toneColors = {
+  "R": "rgb(0, 190, 0)",
+  "2": "rgb(0, 100, 200)",
+  "3": "rgb(210, 255, 0)",
+  "4": "rgb(0, 150, 200)",
+  "5": "rgb(150, 210, 0)",
+  "6": "rgb(180, 0, 250)",
+  "7": "rgb(250, 150, 0)",
+}
 
 let currentChordRootNumber = 0;
 let currentChordInterval = "major";
 let currentInstrument = instruments["guitar"];
-
+let activeTones = ["R", "3", "5"];
 
 function isolate(ctx, fn) {
   ctx.save();
@@ -49,6 +87,13 @@ function isolate(ctx, fn) {
 function onFretBoard(ctx, fn) {
   isolate(ctx, () => {
     ctx.translate(60, 50);
+    fn();
+  })
+}
+
+function onPiano(ctx, fn) {
+  isolate(ctx, () => {
+    ctx.translate(25, 25);
     fn();
   })
 }
@@ -135,9 +180,8 @@ function drawStrings(ctx, instrument) {
   })
 }
 
-function drawTones(ctx, instrument, toneNumber, fillStyle, text) {
+function drawTones(ctx, instrument, toneNumber, label) {
   onFretBoard(ctx, () => {
-    ctx.fillStyle = fillStyle
     ctx.font = "16px serif";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle"
@@ -154,35 +198,237 @@ function drawTones(ctx, instrument, toneNumber, fillStyle, text) {
           const ellipseHeight = 10;
           ctx.beginPath();
           ctx.ellipse(fretXPosition, stringYPosition, ellipseWidth, ellipseHeight, 0, 0, Math.PI * 2);
-          ctx.fillStyle = fillStyle
+          ctx.fillStyle = toneColors[label]
           ctx.fill();
           ctx.stroke();
 
           ctx.fillStyle = "rgb(0, 0, 0)"
-          ctx.fillText(text, fretXPosition, stringYPosition+2)
+          ctx.fillText(label, fretXPosition, stringYPosition+2)
         }
       }
     }
   })
 }
 
-function draw(chordRootNumber, chordInterval, instrument) {
+function drawFretBoard(chordRootNumber, chordInterval, instrument) {
   const canvas = document.getElementById("fretboard-canvas");
-  canvas.width = canvasStyles.canvasWidth * 2;
-  canvas.height = canvasStyles.canvasHeight * 2;
+  canvas.width = canvasStyles.fretboard.width * 2;
+  canvas.height = canvasStyles.fretboard.height * 2;
   if (canvas.getContext) {
     const ctx = canvas.getContext("2d");
     ctx.scale(2, 2);
-
-    const thirdInterval = chordInterval === "major" ? 4 : 3;
 
     drawFrets(ctx, instrument);
     drawStrings(ctx, instrument);
     labelFrets(ctx, instrument);
     labelStrings(ctx, instrument);
-    drawTones(ctx, instrument, chordRootNumber, "rgb(0, 190, 0)", "R");
-    drawTones(ctx, instrument, (chordRootNumber+thirdInterval) % 12, "rgb(210, 255, 0)", "3");
-    drawTones(ctx, instrument,(chordRootNumber+7) % 12, "rgb(150, 210, 0)", "5");
+    activeTones.forEach((activeTone) => {
+      const note = chordPositions[chordInterval].find((note) => note.label === activeTone);
+      drawTones(ctx, instrument, (chordRootNumber+note.position) % 12, note.label);
+    })
+    // drawTones(ctx, instrument, (chordRootNumber+thirdInterval) % 12, "3");
+    // drawTones(ctx, instrument,(chordRootNumber+7) % 12, "5");
+  }
+}
+
+
+const notes = [
+  {
+    label: ["A"],
+    value: 0,
+    accidental: false,
+  }, {
+    label: ["A#","B♭"],
+    value: 1,
+    accidental: true,
+  }, {
+    label: ["B"],
+    value: 2,
+    accidental: false,
+  }, {
+    label: ["C"],
+    value: 3,
+    accidental: false,
+  }, {
+    label: ["C#","D♭"],
+    value: 4,
+    accidental: true,
+  }, {
+    label: ["D"],
+    value: 5,
+    accidental: false,
+  }, {
+    label: ["D#","E♭"],
+    value: 6,
+    accidental: true,
+  }, {
+    label: ["E"],
+    value: 7,
+    accidental: false,
+  }, {
+    label: ["F"],
+    value: 8,
+    accidental: false,
+  }, {
+    label: ["F#","G♭"],
+    value: 9,
+    accidental: true,
+  }, {
+    label: ["G"],
+    value: 10,
+    accidental: false,
+  }, {
+    label: ["G#","A♭"],
+    value: 11,
+    accidental: true,
+  }
+]
+
+
+const pianoStyles = {
+  keyHeight: 60,
+  keyWidth: 46,
+}
+
+function getKeyXPosition(ixKey) {
+  return ixKey * (pianoStyles.keyWidth);
+}
+
+function labelPianoKeys(ctx, chordRootNumber) {
+  const textLineHeight = 20;
+
+  onPiano(ctx, () => {
+    ctx.font = "16px serif";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle"
+
+    const textFillStyles = (accidental) => accidental ? "white" : "black";
+
+    for (let ixKey = 0; ixKey <= 24; ixKey++) {
+      const note = notes[ixKey % 12];
+      const keyXPosition = getKeyXPosition(ixKey);
+
+      ctx.fillStyle = textFillStyles(note.accidental);
+      let textYPosition = pianoStyles.keyHeight/2 - textLineHeight/2 * (note.label.length - 1);
+      note.label.forEach((label, ixLabel) => {
+        ctx.fillText(label, keyXPosition+(pianoStyles.keyWidth/2), (textYPosition + ixLabel*textLineHeight))
+      })
+    }
+  })
+}
+
+function drawPianoKeys(ctx, chordRootNumber) {
+  onPiano(ctx, () => {
+    const accidentalStyles = (faded) => faded ? "gray" : "black";
+    const naturalStyles = (faded) => faded ? "lightgray" : "ivory";
+
+    for (let ixKey = 0; ixKey <= 24; ixKey++) {
+      const note = notes[ixKey % 12];
+      const keyXPosition = getKeyXPosition(ixKey);
+
+      let faded = true;
+      if (chordRootNumber <= ixKey && ixKey <= chordRootNumber + 12) {
+        faded = false;
+      }
+
+      ctx.beginPath()
+      ctx.rect(keyXPosition, 0, pianoStyles.keyWidth, pianoStyles.keyHeight)
+      ctx.fillStyle = note.accidental ? accidentalStyles(faded) : naturalStyles(faded);
+      ctx.fill();
+      ctx.strokeStyle = "rgba(100, 100, 100, 1)";
+      ctx.stroke();
+    }
+  })
+}
+
+function getRandomColor() {
+  const r = Math.round(Math.random() * 255);
+  const g = Math.round(Math.random() * 255);
+  const b = Math.round(Math.random() * 255);
+  return `rgb(${r},${g},${b})`;
+}
+
+const hitMap = {};
+
+function drawTonesOnPiano(ctx, hitCtx, chordRootNumber, chordInterval) {
+  const ellipseWidth = 16;
+  const ellipseHeight = 10;
+
+  onPiano(ctx, () => {
+    ctx.font = "16px serif";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle"
+
+    chordPositions[chordInterval].forEach((chordPosition) => {
+      const x = getKeyXPosition(chordRootNumber + chordPosition.position) + pianoStyles.keyWidth / 2;
+      const y = pianoStyles.keyHeight + 20;
+      ctx.beginPath();
+      ctx.ellipse(x, y, ellipseWidth, ellipseHeight, 0, 0, Math.PI * 2);
+      ctx.fillStyle = activeTones.includes(chordPosition.label) ? toneColors[chordPosition.label] : "lightgray";
+      ctx.fill();
+      ctx.strokeStyle = activeTones.includes(chordPosition.label) ? "black" : "gray";
+      ctx.stroke();
+
+      ctx.fillStyle = "rgb(0, 0, 0)"
+      ctx.fillText(chordPosition.label, x, y+2)
+    });
+  })
+
+  onPiano(hitCtx, () => {
+    chordPositions[chordInterval].forEach((chordPosition) => {
+      const x = getKeyXPosition(chordRootNumber + chordPosition.position) + pianoStyles.keyWidth / 2;
+      const y = pianoStyles.keyHeight + 20;
+
+      const hitColor = getRandomColor();
+      hitCtx.beginPath();
+      hitCtx.ellipse(x, y, ellipseWidth+3, ellipseHeight+3, 0, 0, Math.PI * 2);
+      hitCtx.fillStyle = hitColor;
+      hitCtx.fill();
+
+      hitMap[hitColor] = chordPosition;
+    });
+  })
+}
+
+function pianoClickHandler(pianoCanvas, pianoCtx, hitCtx) {
+  return function handlePianoClick(event) {
+    const canvasBox = pianoCanvas.getBoundingClientRect();
+    const x = event.clientX - canvasBox.left;
+    const y = event.clientY - canvasBox.top;
+    const pixel = hitCtx.getImageData(x*2, y*2, 1, 1).data;
+    const clickedColor = `rgb(${pixel[0]},${pixel[1]},${pixel[2]})`;
+    const clickedTone = hitMap[clickedColor]?.label || null;
+    if (clickedTone) {
+      if (activeTones.includes(clickedTone)) {
+        activeTones = activeTones.filter((tone) => tone !== clickedTone);
+      } else {
+        activeTones.push(clickedTone);
+      }
+      drawFretBoard(currentChordRootNumber, currentChordInterval, currentInstrument);
+      pianoCanvas.outerHTML = pianoCanvas.outerHTML;
+      drawPiano(currentChordRootNumber, currentChordInterval);
+    }
+  }
+}
+
+function drawPiano(chordRootNumber, chordInterval) {
+  const pianoCanvas = document.getElementById("piano-canvas");
+  const hitCanvas = document.getElementById("hit-canvas");
+  pianoCanvas.width = canvasStyles.piano.width * 2;
+  pianoCanvas.height = canvasStyles.piano.height * 2;
+  hitCanvas.width = canvasStyles.piano.width * 2;
+  hitCanvas.height = canvasStyles.piano.height * 2;
+
+  if (pianoCanvas.getContext && hitCanvas.getContext) {
+    const ctx = pianoCanvas.getContext("2d");
+    const hitCtx = hitCanvas.getContext("2d");
+    ctx.scale(2, 2);
+    hitCtx.scale(2, 2);
+
+    drawPianoKeys(ctx, chordRootNumber);
+    labelPianoKeys(ctx, chordRootNumber);
+    drawTonesOnPiano(ctx, hitCtx, chordRootNumber, chordInterval);
+    pianoCanvas.addEventListener('click', pianoClickHandler(pianoCanvas, ctx, hitCtx));
   }
 }
 
@@ -206,15 +452,24 @@ function handleChordSelect(chordRoot, interval) {
       currentChordInterval = interval;
     }
     currentChordRootNumber = chordRootNumbers[chordRoot];
-    setTimeout(()=>draw(currentChordRootNumber, currentChordInterval, currentInstrument), 0);
+    setTimeout(()=>{
+        drawFretBoard(currentChordRootNumber, currentChordInterval, currentInstrument);
+        drawPiano(currentChordRootNumber, currentChordInterval);
+      }, 0)
+    ;
   }
 }
 
 function handleInstrumentSelect(instrumentName) {
   if (instruments.hasOwnProperty(instrumentName)) {
     currentInstrument = instruments[instrumentName];
-    setTimeout(()=>draw(currentChordRootNumber, currentChordInterval, currentInstrument), 0);
+    setTimeout(() => drawFretBoard(currentChordRootNumber, currentChordInterval, currentInstrument), 0);
   }
 }
 
-draw(0, "major", instruments["guitar"])
+drawFretBoard(0, "major", instruments["guitar"])
+drawPiano(0, "major")
+
+
+window.handleChordSelect = handleChordSelect;
+window.handleInstrumentSelect = handleInstrumentSelect;
