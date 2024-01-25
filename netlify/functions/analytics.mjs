31cf7e0ded484183e9e6c4095a6a8e06fa070017
Tyler Vick <vick.tyler@gmail.com>
2024-01-25 02:28:47 -0500

    more logs

--- netlify/functions/analytics.mjs
+++ netlify/functions/analytics.mjs
@@ -2,7 +2,12 @@ import {BigQuery} from "@google-cloud/bigquery";
 import {GoogleAuth} from "google-auth-library";
 
 import * as acctKey from "./service_account_key.json";
+
 console.log(acctKey);
+console.log({
+  client_email: acctKey.client_email,
+  private_key: acctKey.private_key,
+})
 
 const datasetId = Netlify.env.get("BIGQUERY_DATASET_ID");
 const tableId = Netlify.env.get("BIGQUERY_ANALYTICS_TABLE_ID");
