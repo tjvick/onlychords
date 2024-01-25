5ff4c2ae5e7b7f889668f3767e6ca239f1530288
Tyler Vick <vick.tyler@gmail.com>
2024-01-25 02:50:13 -0500

    add a project id to the google auth object

--- netlify/functions/analytics.mjs
+++ netlify/functions/analytics.mjs
@@ -7,10 +7,13 @@ export const handler = async (req, context) => {
   const datasetId = process.env.BIGQUERY_DATASET_ID;
   const tableId = process.env.BIGQUERY_ANALYTICS_TABLE_ID;
 
+  console.log(acctKey);
+
   const googleAuth = new GoogleAuth({
     credentials: {
       client_email: acctKey.client_email,
       private_key: acctKey.private_key,
+      projectId: acctKey.project_id,
     },
     scopes: 'https://www.googleapis.com/auth/cloud-platform',
   });
