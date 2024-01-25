97bda8a12e384813b3561d941e4c4e06b8c402ee
Tyler Vick <vick.tyler@gmail.com>
2024-01-25 02:17:33 -0500

    build credentials explicitly

--- netlify/functions/analytics.mjs
+++ netlify/functions/analytics.mjs
@@ -1,10 +1,23 @@
 import {BigQuery} from "@google-cloud/bigquery";
+import {GoogleAuth} from "google-auth-library";
+
 import * as acctKey from "./service_account_key.json";
 
 const datasetId = Netlify.env.get("BIGQUERY_DATASET_ID");
 const tableId = Netlify.env.get("BIGQUERY_ANALYTICS_TABLE_ID");
 
-const bigQueryClient = new BigQuery();
+const googleAuth = new GoogleAuth({
+  credentials: {
+    client_email: acctKey.client_email,
+    private_key: acctKey.private_key,
+  },
+  scopes: 'https://www.googleapis.com/auth/cloud-platform',
+});
+
+const bigQueryClient = new BigQuery({
+  projectId: googleAuth.getProjectId(),
+  authClient: googleAuth
+});
 
 async function handler(req, context) {
   const rows = [
