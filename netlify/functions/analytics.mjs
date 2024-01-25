2054284378c0990bdb23adf964a945b991b1bbf3
Tyler Vick <vick.tyler@gmail.com>
2024-01-25 02:05:05 -0500

    relative import

--- netlify/functions/analytics.mjs
+++ netlify/functions/analytics.mjs
@@ -1,5 +1,5 @@
 import {BigQuery} from "@google-cloud/bigquery";
-import * as acctKey from "service_account_key.json";
+import * as acctKey from "./service_account_key.json";
 
 const datasetId = Netlify.env.get("BIGQUERY_DATASET_ID");
 const tableId = Netlify.env.get("BIGQUERY_ANALYTICS_TABLE_ID");
