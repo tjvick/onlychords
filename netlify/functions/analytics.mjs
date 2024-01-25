2c1f2db6ec4ec23d6cd55e764d6efb2d3eb4fe0d
Tyler Vick <vick.tyler@gmail.com>
2024-01-25 02:20:00 -0500

    create dummy sa key file

--- netlify/functions/analytics.mjs
+++ netlify/functions/analytics.mjs
@@ -2,6 +2,7 @@ import {BigQuery} from "@google-cloud/bigquery";
 import {GoogleAuth} from "google-auth-library";
 
 import * as acctKey from "./service_account_key.json";
+console.log(acctKey);
 
 const datasetId = Netlify.env.get("BIGQUERY_DATASET_ID");
 const tableId = Netlify.env.get("BIGQUERY_ANALYTICS_TABLE_ID");
